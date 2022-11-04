e9ac143ce7157d3f5542173520008f4a4c6a8163
Victoran0 <victoranosunkvic@gmail.com>
2022-11-04 19:37:54 +0100

    Fourth Commit

--- deliver/settings.py
+++ deliver/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,7 +136,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
